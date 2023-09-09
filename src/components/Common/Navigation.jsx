e93c9ab50d4ac4283757865dd6fcd56787079f57
Muhammad Fakhryan Zulfahmi <fakhryandev@gmail.com>
2023-09-10 00:58:02 +0700

    change brand nav

--- src/components/Common/Navigation.jsx
+++ src/components/Common/Navigation.jsx
@@ -27,7 +27,7 @@ function Navigation() {
     <nav className="navbar container mx-auto text-black font-bold">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost normal-case text-xl">
-          Society App
+          Society
         </Link>
       </div>
       <div className="flex-none">
